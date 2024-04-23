ac7205fd4dd1c0591d4f7fe18fa25a4dec0d223d
GiantRat985 <156049816+GiantRat985@users.noreply.github.com>
2024-04-22 21:55:42 -1000

    refactor: use primary constructor
    Refactor recipe class to use primary constructor.

--- src/Models/Recipe.cs
+++ src/Models/Recipe.cs
@@ -3,36 +3,27 @@
     /// <summary>
     /// Holds information about a recipe.
     /// </summary>
-    public class Recipe
+    public class Recipe(string title, string? url, string? description, string? body, byte[]? thumbnail)
     {
         /// <summary>
         /// Recipe title.
         /// </summary>
-        public string Title { get; }
+        public string Title { get; } = title;
         /// <summary>
         /// Holds the URL for a recipe if the recipe is gathered from the internet.
         /// </summary>
-        public string? URL { get; }
+        public string? URL { get; } = url;
         /// <summary>
         /// Description of the recipe; time to cook, servings, summary.
         /// </summary>
-        public string? Description { get; }
+        public string? Description { get; } = description;
         /// <summary>
         /// Main body of the recipe; directions, ingredients, notes.
         /// </summary>
-        public string? Body { get; }
+        public string? Body { get; } = body;
         /// <summary>
         /// Optional thumbnail image, saved as a byte array.
         /// </summary>
-        public byte[]? Thumbnail { get; }
-
-        public Recipe(string title, string? url, string? description, string? body, byte[]? thumbnail)
-        {
-            Title = title;
-            URL = url;
-            Description = description;
-            Body = body;
-            Thumbnail = thumbnail;
-        }
+        public byte[]? Thumbnail { get; } = thumbnail;
     }
 }
