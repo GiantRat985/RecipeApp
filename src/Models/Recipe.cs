ed6b2d08eb649fab1bd6aa06a055f4f816422afd
GiantRat985 <156049816+GiantRat985@users.noreply.github.com>
2024-04-22 21:35:46 -1000

    feat: add models directory
    Add new directory for the models layer.
    
    Add recipe class to hold information on submitted recipes.

---
+++ src/Models/Recipe.cs
@@ -0,0 +1,38 @@
+﻿namespace RecipeApp
+{
+    /// <summary>
+    /// Holds information about a recipe.
+    /// </summary>
+    public class Recipe
+    {
+        /// <summary>
+        /// Recipe title.
+        /// </summary>
+        public string Title { get; }
+        /// <summary>
+        /// Holds the URL for a recipe if the recipe is gathered from the internet.
+        /// </summary>
+        public string? URL { get; }
+        /// <summary>
+        /// Description of the recipe; time to cook, servings, summary.
+        /// </summary>
+        public string? Description { get; }
+        /// <summary>
+        /// Main body of the recipe; directions, ingredients, notes.
+        /// </summary>
+        public string? Body { get; }
+        /// <summary>
+        /// Optional thumbnail image, saved as a byte array.
+        /// </summary>
+        public byte[]? Thumbnail { get; }
+
+        public Recipe(string title, string? url, string? description, string? body, byte[]? thumbnail)
+        {
+            Title = title;
+            URL = url;
+            Description = description;
+            Body = body;
+            Thumbnail = thumbnail;
+        }
+    }
+}
