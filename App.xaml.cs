d60a0d3c42cd1e12237c9d2f5e1d277fe6b33c23
GiantRat985 <156049816+GiantRat985@users.noreply.github.com>
2024-04-25 15:50:47 -1000

    feat: add service provider
    Add service provider to App code-behind to handle dependency injection.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace RecipeApp
 {
@@ -9,6 +11,40 @@ namespace RecipeApp
     /// </summary>
     public partial class App : Application
     {
+        private string CONNECTION_STRING = "Data Source=recipeapp.db";
+        private IServiceProvider? _serviceProvider;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            CreateDbContexts();
+
+            CreateServiceProvider();
+
+            var mainWindow = _serviceProvider!.GetService<MainWindow>();
+            mainWindow!.Show();
+        }
+
+        private void CreateServiceProvider()
+        {
+            var serviceCollection = new ServiceCollection()
+                .AddSingleton<MainWindow>()
+
+                .AddTransient<RecipeAppDbContextFactory>()
+                ;
+            
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        private void CreateDbContexts()
+        {
+            DbContextOptions options = new DbContextOptionsBuilder().UseSqlite(CONNECTION_STRING).Options;
+            using (RecipeAppDbContext context  = new RecipeAppDbContext(options))
+            {
+                context.Database.Migrate();
+            }
+        }
     }
 
 }
