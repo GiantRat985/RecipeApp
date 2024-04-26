1ae2eec89657e9dbc341576ddc29b3a4250ba60a
GiantRat985 <156049816+GiantRat985@users.noreply.github.com>
2024-04-25 15:50:07 -1000

    feat: add dto classes
    Add data transfer object classes to transfer recipe data to the database cleanly.

---
+++ src/DbContext/IRecipeDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Policy;
+
+namespace RecipeApp
+{
+    public interface IRecipeDTO
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string? URL { get; set; }
+        public string? Description { get; set; }
+        public string? Body { get; set; }
+        public byte[]? Thumbnail { get; set; }
+    }
+}
---
+++ src/DbContext/RecipeDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace RecipeApp
+{
+    public class RecipeDTO : IRecipeDTO
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string? URL { get; set; }
+        public string? Description { get; set; }
+        public string? Body { get; set; }
+        public byte[]? Thumbnail { get; set; }
+    }
+}
