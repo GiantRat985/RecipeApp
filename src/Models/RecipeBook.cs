febd6be8fbe83d98dd79a26c6728d5620d48d979
GiantRat985 <156049816+GiantRat985@users.noreply.github.com>
2024-04-26 20:20:28 -1000

    refactor: add repository class
    Add repository class to centralize CRUD operations to the database.

--- src/Models/RecipeBook.cs
+++ src/Models/RecipeBook.cs
@@ -1,7 +1,30 @@
 ﻿namespace RecipeApp
 {
-    public class RecipeBook
+    public class RecipeBook(IRecipeCreator recipeCreator, IRecipeProvider recipeProvider, IRecipeUpdater recipeUpdater, IRecipeDeleter recipeDeleter)
     {
+        private readonly IRecipeCreator _recipeCreator = recipeCreator;
+        private readonly IRecipeProvider _recipeProvider = recipeProvider;
+        private readonly IRecipeUpdater _recipeUpdater = recipeUpdater;
+        private readonly IRecipeDeleter _recipeDeleter = recipeDeleter;
 
+        public async Task CreateEntry(IRecipe recipe)
+        {
+            await _recipeCreator.CreateRecipe(recipe);
+        }
+
+        public async Task<IEnumerable<RecipeDTO>> GetAllEntries()
+        {
+            return await _recipeProvider.GetAllRecipes();
+        }
+
+        public async Task UpdateEntry(IRecipe recipe)
+        {
+            await _recipeUpdater.UpdateDatabase(recipe);
+        }
+
+        public async Task DeleteEntry(int id)
+        {
+            await _recipeDeleter.DeleteRecipe(id);
+        }
     }
 }
