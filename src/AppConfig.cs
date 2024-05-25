e08078f5e2c4c2bdc0035da491befecd08cd5bc5
GiantRat985 <156049816+GiantRat985@users.noreply.github.com>
2024-05-24 17:43:31 -1000

    refactor: add options classes
    Fix the appsettings.json integration

---
+++ src/AppConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace RecipeApp
+{
+    public static class AppConfig
+    {
+        public static Settings Settings { get; private set; } = new Settings();
+
+        public static void LoadCongifuration(string path)
+        {
+            var configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile(path, optional: false, reloadOnChange: true)
+               .Build();
+
+            configuration.GetSection(Settings.Key).Bind(Settings);
+        }
+    }
+}
---
+++ src/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeApp
+{
+    public class Settings
+    {
+        public const string Key = "AppSettings";
+        public string DbConnectionString { get; set; }
+    }
+}
