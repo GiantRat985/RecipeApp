3ddf62e5ae92316e4e23b1b09747fdb71a60334d
GiantRat985 <156049816+GiantRat985@users.noreply.github.com>
2024-06-02 22:15:32 -1000

    refactor: add simplified null check into the parsing exception class
    Add a ThrowIfNull static method into the ParsingFailureException class to simplify null checks.

---
+++ exceptions/ParsingFailureException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeApp.Exceptions
+{
+    /// <summary>
+    /// Represents errors during parsing.
+    /// </summary>
+    public class ParsingFailureException : Exception
+    {
+        public ParsingFailureException()
+        {
+            
+        }
+
+        public ParsingFailureException(string message) : base(message)
+        {
+            
+        }
+
+        public ParsingFailureException(string message, Exception inner) : base(message, inner)
+        {
+            
+        }
+
+        public static void ThrowIfNull(object? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        {
+            if (argument is null)
+            {
+                throw new ParsingFailureException();
+            }
+        }
+    }
+}
