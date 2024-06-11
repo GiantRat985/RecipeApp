86b00fe25bf738788cffbf210162bd93de110ad8
GiantRat985 <156049816+GiantRat985@users.noreply.github.com>
2024-06-11 12:40:41 -1000

    feat: add new column to database
    Update the database to hold the byte array data for an image of each recipe.

---
+++ services/database/migrations/20240610034828_AddRecipeImage.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRecipeImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RecipeImage",
+                table: "RecipeSet",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecipeImage",
+                table: "RecipeSet");
+        }
+    }
+}
